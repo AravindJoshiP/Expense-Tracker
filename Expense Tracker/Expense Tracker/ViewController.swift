a1346a35622fba5c4f91b044b6a33bd47f006144
Aravind Joshi P <apenumak@depaul.edu>
2026-03-03 16:32:23 -0600

    Created a Project

---
+++ Expense Tracker/Expense Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expense Tracker
+//
+//  Created by Aravind Josh on 03/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
