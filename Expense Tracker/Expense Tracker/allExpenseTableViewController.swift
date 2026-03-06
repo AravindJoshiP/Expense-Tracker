b74a2fc7ab3f8b56a148eee6edd9905315796a17
Aravind - MAC <apenumak@depaul.edu>
2026-03-06 15:39:26 -0600

    added delete feature

--- Expense Tracker/Expense Tracker/allExpenseTableViewController.swift
+++ Expense Tracker/Expense Tracker/allExpenseTableViewController.swift
@@ -59,17 +59,18 @@ class allExpenseTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
+            expenses.remove(at: indexPath.row)
+            
+        } //else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        //}    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
